b9f2d9b63f2691a694890545bf1e9ce29ca22f16
Ondrej Pilous <htcandreassaw@gmail.com>
2025-07-18 22:18:35 +0200

    New plugins: Treesitter, ts-autotag

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,63 @@
+return ({{
+		"nvim-treesitter/nvim-treesitter",
+		version = false, -- last release is way too old and doesn't work on Windows
+		run = ":TSUpdate",
+		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+		keys = {
+			{ "<c-space>", desc = "Increment Selection" },
+			{ "<bs>",      desc = "Decrement Selection", mode = "x" },
+		},
+		opts_extend = { "ensure_installed" },
+		opts = {
+			highlight = {
+				enable = true, -- false will disable the whole extension
+			},
+			indent = { enable = true },
+			ensure_installed = {
+				"bash",
+				"c",
+				"css",
+				"diff",
+				"html",
+				"javascript",
+				"jsdoc",
+				"json",
+				"jsonc",
+				"lua",
+				"luadoc",
+				"luap",
+				"markdown",
+				"markdown_inline",
+				"printf",
+				"python",
+				"query",
+				"regex",
+				"toml",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"vue",
+				"xml",
+				"yaml",
+			},
+			incremental_selection = {
+			    enable = true,
+			    keymaps = {
+				init_selection = "<C-space>",
+				node_incremental = "<C-space>",
+				scope_incremental = false,
+				node_decremental = "<bs>",
+			    }
+			},
+			autotag = {
+			    enable = true
+			}
+		},
+	},
+	{
+	    "windwp/nvim-ts-autotag",
+	    event = "InsertEnter"
+	}
+
+    })
